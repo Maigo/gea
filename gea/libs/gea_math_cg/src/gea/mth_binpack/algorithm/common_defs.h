274bb80c1ef23c1c3962434478efe948dcc20108
Teodor Jakobsson <gomenne@gmail.com>
2016-10-31 20:09:40 +0100

    rename gea_math_graph to more generic computational geometry lib. added skeleton for bin pack algorithms.

---
+++ gea/libs/gea_math_cg/src/gea/mth_binpack/algorithm/common_defs.h
@@ -0,0 +1,14 @@
+#ifndef __GEA_MTH_BINPACK_COMMON_DEFS_H__
+#define __GEA_MTH_BINPACK_COMMON_DEFS_H__
+
+namespace gea {
+namespace mth {
+
+// ------------------------------------------------------------------------- //
+// common defs                                                               //
+// ------------------------------------------------------------------------- //
+
+} // namespace mth //
+} // namespace gea //
+
+#endif // __GEA_MTH_BINPACK_COMMON_DEFS_H__ //
