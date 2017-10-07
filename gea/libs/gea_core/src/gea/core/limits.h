767239a8d4425ee05bad3317e87207734bb34a14
Teodor Jakobsson <gomenne@gmail.com>
2017-10-07 15:55:48 +0200

    added std limits alias.

---
+++ gea/libs/gea_core/src/gea/core/limits.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// std includes
+#include <limits>
+
+namespace gea {
+
+// ------------------------------------------------------------------------- //
+// limits                                                                    //
+// ------------------------------------------------------------------------- //
+
+// alias templates
+template <typename T>
+using numeric_limits = std::numeric_limits<T>;
+
+// ------------------------------------------------------------------------- //
+
+} // namespace gea //
